bb4f34264c2cef07a466b1ad17d7ca90571d2325
Michael Bumann <hello@michaelbumann.com>
2024-03-02 15:31:00 +0100

    Add dylib for macos

--- ldk_node/cgo.go
+++ ldk_node/cgo.go
@@ -3,5 +3,7 @@ package ldk_node
 /*
 #cgo LDFLAGS: -lldk_node
 #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
+#cgo darwin,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
+#cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
 */
 import "C"
