c52ea2f79aeaa3b5c96e2117db98cf96fb758c05
Roman Dmitrienko <roman@dmitrienko.com>
2024-04-16 19:43:07 +0200

    Update ARM6 hardfloat library path in cgo.go.

--- ldk_node/cgo.go
+++ ldk_node/cgo.go
@@ -5,6 +5,6 @@ package ldk_node
 #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/x86_64-unknown-linux-gnu -L${SRCDIR}/x86_64-unknown-linux-gnu
 #cgo darwin LDFLAGS: -Wl,-rpath,${SRCDIR}/universal-macos -L${SRCDIR}/universal-macos
 #cgo windows,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/x86_64-pc-windows-gnu -L${SRCDIR}/x86_64-pc-windows-gnu
-#cgo linux,arm.6 LDFLAGS: -Wl,-rpath,${SRCDIR}/arm-unknown-linux-gnueabi -L${SRCDIR}/arm-unknown-linux-gnueabi
+#cgo linux,arm.6 LDFLAGS: -Wl,-rpath,${SRCDIR}/arm-unknown-linux-gnueabihf -L${SRCDIR}/arm-unknown-linux-gnueabihf
 */
 import "C"
